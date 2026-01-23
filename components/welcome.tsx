294fcb7695504bd37ce3d89a32fcc03af510577a
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-01-23 22:31:20 +0300

    test(viewport): safe area

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,20 +1,30 @@
 'use client';
 
-import {useLaunchParams, useAndroidDeviceData, useRawInitData} from '@tma.js/sdk-react';
+import {useLaunchParams, useAndroidDeviceData, useRawInitData, viewport, useSignal} from '@tma.js/sdk-react';
+import {useLayoutEffect} from "react";
 
 export const Welcome = () => {
     const launchParams = useLaunchParams();
     const androidDeviceData = useAndroidDeviceData();
     const rawInitData = useRawInitData();
 
+    useLayoutEffect(() => {
+        (async () => {
+            await viewport.mount();
+        })();
+    }, []);
+
     const {tgWebAppData} = launchParams;
+    const marginTop = useSignal(viewport.contentSafeAreaInsetTop);
 
     if (!tgWebAppData || !tgWebAppData.user) return null;
 
     const {first_name, last_name, username, photo_url} = tgWebAppData.user;
     const {model, manufacturer} = androidDeviceData;
 
+
     return <div>
+        <div style={{marginTop: marginTop}}></div>
         <h1>Welcome, {first_name} {last_name}</h1>
         <p style={{fontSize: '8px'}}>@{username}</p>
 
