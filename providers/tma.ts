77ada5fb8a976867da49a29507de2d02ce5a9606
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-01-23 23:23:13 +0300

    feat(area): add safe area

--- providers/tma.ts
+++ providers/tma.ts
@@ -1,12 +1,17 @@
 'use client';
 
-import {init, viewport} from '@tma.js/sdk-react';
+import {init, viewport, requestWriteAccess} from '@tma.js/sdk-react';
 import {useEffect} from 'react';
 
 export function TmaInit() {
     useEffect(() => {
         init();
         viewport.mount();
+        (async () => {
+            const status = await requestWriteAccess();
+            alert('status:' + status);
+        })();
+
     }, []);
 
     return null;
