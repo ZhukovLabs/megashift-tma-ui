1003cea4939fea30fb63398a585368231edeb244
Denis Zhukov <dzianis.zhukau@modsen-software.com>
2026-01-24 20:27:52 +0300

    test: next-intl

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -2,11 +2,13 @@
 
 import {useLaunchParams, useAndroidDeviceData, useRawInitData, viewport, useSignal} from '@tma.js/sdk-react';
 import {useLayoutEffect} from "react";
+import {useMessages} from "next-intl";
 
 export const Welcome = () => {
     const launchParams = useLaunchParams();
     const androidDeviceData = useAndroidDeviceData();
     const rawInitData = useRawInitData();
+    const messages = useMessages();
 
     useLayoutEffect(() => {
         (async () => {
@@ -20,17 +22,14 @@ export const Welcome = () => {
     if (!tgWebAppData || !tgWebAppData.user) return null;
 
     const {first_name, last_name, username, photo_url} = tgWebAppData.user;
-    const {model, manufacturer} = androidDeviceData;
 
 
     return <div>
         <div style={{marginTop: marginTop}}></div>
-        <h1>Welcome, {first_name} {last_name}</h1>
+        <h1>{messages.welcome}, {first_name} {last_name}</h1>
         <p style={{fontSize: '8px'}}>@{username}</p>
 
         <img src={photo_url} alt="avatar"/>
-
-        <h2>You enter from {manufacturer} {model}</h2>
 
         <p>RawInitData:</p>
         <code>{rawInitData}</code>
