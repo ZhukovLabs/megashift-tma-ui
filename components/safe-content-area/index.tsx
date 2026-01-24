bd38510c60a674cf13aa4f2273786d657f0617e1
Denis Zhukov <dzianis.zhukau@modsen-software.com>
2026-01-24 20:24:34 +0300

    feat: add tailwind

--- components/safe-content-area/index.tsx
+++ components/safe-content-area/index.tsx
@@ -1,18 +1,34 @@
 'use client';
 
 import {useSignal, viewport} from "@tma.js/sdk-react";
+import {clsx} from "clsx";
 
 type SafeContentAreaProps = {
     children: React.ReactNode;
+    className?: string;
 }
 
-export const SafeContentArea = ({children}: SafeContentAreaProps) => {
+const SIDE_PADDINGS = 5;
+
+export const SafeContentArea = ({children, className}: SafeContentAreaProps) => {
     const {top, left, right, bottom} = useSignal(viewport.contentSafeAreaInsets);
 
-    return <div style={{
-        padding: `${top}px ${right+5}px ${bottom}px ${left+5}px`,
-        maxWidth: `calc(100vw - ${left}px - ${right}px)`
-    }}>
-        {children}
-    </div>
-}
+    const dynamicStyles = {
+        padding: `${top}px ${right + SIDE_PADDINGS}px ${bottom}px ${left + SIDE_PADDINGS}px`,
+        maxWidth: `calc(100vw - ${left}px - ${right}px)`,
+    };
+
+    return (
+        <div
+            style={dynamicStyles}
+            className={clsx(
+                "overflow-x-auto",
+                "w-full",
+                "md:overflow-x-visible",
+                className
+            )}
+        >
+            {children}
+        </div>
+    );
+};
