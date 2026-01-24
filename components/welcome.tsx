c743615bc52f4eaa733a3881ebb7722d33634797
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-01-24 21:43:28 +0300

    feat: remove useless code

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,23 +1,14 @@
 'use client';
 
-import {useLaunchParams, useAndroidDeviceData, useRawInitData, viewport, useSignal} from '@tma.js/sdk-react';
-import {useLayoutEffect} from "react";
+import {useLaunchParams, useRawInitData} from '@tma.js/sdk-react';
 import {useMessages} from "next-intl";
 
 export const Welcome = () => {
     const launchParams = useLaunchParams();
-    const androidDeviceData = useAndroidDeviceData();
     const rawInitData = useRawInitData();
     const messages = useMessages();
 
-    useLayoutEffect(() => {
-        (async () => {
-            await viewport.mount();
-        })();
-    }, []);
-
     const {tgWebAppData} = launchParams;
-    const marginTop = useSignal(viewport.contentSafeAreaInsetTop);
 
     if (!tgWebAppData || !tgWebAppData.user) return null;
 
@@ -25,7 +16,6 @@ export const Welcome = () => {
 
 
     return <div>
-        <div style={{marginTop: marginTop}}></div>
         <h1>{messages.welcome}, {first_name} {last_name}</h1>
         <p style={{fontSize: '8px'}}>@{username}</p>
 
