3968fbf821dcd220d2b7f9421de3a86b1cce0dad
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-02-15 22:50:03 +0300

    feat: popup is expired

--- hooks/use-process-invite.ts
+++ hooks/use-process-invite.ts
@@ -1,24 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
-import { popup } from '@tma.js/sdk';
-import { useCheckInvite, useConsumeInvite } from '@/api-hooks/users/invites';
+import {useEffect, useRef, useState} from 'react';
+import {popup} from '@tma.js/sdk';
+import {useCheckInvite, useConsumeInvite} from '@/api-hooks/users/invites';
+import {useUserStore} from "@/store/user-store";
 
 type UseProcessInviteParams = {
     inviteId?: string | null;
     isLoadingUser: boolean;
 };
 
-export const useProcessInvite = ({ inviteId, isLoadingUser }: UseProcessInviteParams) => {
+export const useProcessInvite = ({inviteId}: UseProcessInviteParams) => {
+    const user = useUserStore(s => s.user);
     const hasProcessedRef = useRef(false);
 
-    const { mutateAsync: checkInviteAsync } = useCheckInvite();
-    const { mutateAsync: consumeInviteAsync } = useConsumeInvite();
+    const {mutateAsync: checkInviteAsync} = useCheckInvite();
+    const {mutateAsync: consumeInviteAsync} = useConsumeInvite();
 
     const [isProcessing, setIsProcessing] = useState(false);
     const [inviteHandled, setInviteHandled] = useState(false);
     const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
-        if (isLoadingUser || hasProcessedRef.current) return;
+        if (!user || hasProcessedRef.current) return;
 
         hasProcessedRef.current = true;
 
@@ -34,11 +36,15 @@ export const useProcessInvite = ({ inviteId, isLoadingUser }: UseProcessInvitePa
                 const response = await checkInviteAsync(inviteId);
 
                 if (!response?.exists) {
+                    await popup.show({
+                        title: "Приглашение",
+                        message: "Приглашение устарело"
+                    });
                     setInviteHandled(true);
                     return;
                 }
 
-                const { inviter: { surname, name, patronymic }, claims } = response;
+                const {inviter: {surname, name, patronymic}, claims} = response;
                 const fullName = [surname, name, patronymic].filter(Boolean).join(' ');
                 const message = `${fullName} пригласил вас к своему расписанию\n\nДоступ: ${claims.join(', ')}`;
 
@@ -46,8 +52,8 @@ export const useProcessInvite = ({ inviteId, isLoadingUser }: UseProcessInvitePa
                     title: 'Приглашение',
                     message,
                     buttons: [
-                        { id: 'accept', type: 'default', text: 'Принять' },
-                        { id: 'decline', type: 'destructive', text: 'Отклонить' },
+                        {id: 'accept', type: 'default', text: 'Принять'},
+                        {id: 'decline', type: 'destructive', text: 'Отклонить'},
                     ],
                 });
 
@@ -63,7 +69,7 @@ export const useProcessInvite = ({ inviteId, isLoadingUser }: UseProcessInvitePa
         };
 
         processInvite();
-    }, [inviteId, isLoadingUser]);
+    }, [inviteId, user]);
 
-    return { isProcessing, inviteHandled, error };
+    return {isProcessing, inviteHandled, error};
 };
