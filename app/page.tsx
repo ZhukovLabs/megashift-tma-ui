9470ef7698c8b9e707b49a78bb88e68e0a749b64
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-02-16 00:01:04 +0300

    feat: update root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,39 @@
 'use client';
 
-import {useSyncRegisteredUser} from '@/components/start-form/hooks/use-sync-registered-user';
+import {useEffect, useMemo} from 'react';
 import {useSearchParams, useRouter} from 'next/navigation';
 import {ROUTES} from '@/constants/routes';
 import {useUserStore} from '@/store/user-store';
-import {useEffect} from 'react';
-import {usePrefetch} from "@/hooks/use-prefetch";
+import {useSyncRegisteredUser} from '@/components/start-form/hooks/use-sync-registered-user';
+import {usePrefetch} from '@/hooks/use-prefetch';
 
 const RootPage = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
-    const user = useUserStore(s=>s.user);
+    const user = useUserStore((s) => s.user);
 
     const {isLoading} = useSyncRegisteredUser();
 
-    const startapp = searchParams.get('startapp') ?? null;
+    const startapp = searchParams.get('startapp');
     const redirect = searchParams.get('redirect');
 
+    usePrefetch({urls: [ROUTES.onboarding, ROUTES.schedule]});
+
+    const redirectUrl = useMemo(() => {
+        if (isLoading) return null;
+
+        const target = user ? redirect || ROUTES.schedule : ROUTES.onboarding;
+        const url = new URL(target, window.location.origin);
+
+        if (startapp) url.searchParams.set('startapp', startapp);
+        return url.pathname + url.search;
+    }, [isLoading, user, redirect, startapp]);
+
     useEffect(() => {
-        if (isLoading) return;
-
-        const url = new URL(user ? (redirect || ROUTES.schedule) : ROUTES.onboarding, window.location.origin);
-        if (startapp) {
-            url.searchParams.set('startapp', startapp);
+        if (redirectUrl) {
+            router.replace(redirectUrl);
         }
-
-        router.replace(url.pathname + url.search);
-    }, [isLoading, redirect, router, startapp, user]);
-
-    usePrefetch({urls: [ROUTES.schedule]});
+    }, [redirectUrl, router]);
 
     return (
         <div className="flex h-screen items-center justify-center">
