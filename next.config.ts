3fe48c37ccb6843c3f7329ce48ce277eccde4b52
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-02-22 20:29:28 +0300

    feat: health check is added

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/index.ts');
 
 const nextConfig: NextConfig = {
+
 };
 
 export default withNextIntl(nextConfig);
